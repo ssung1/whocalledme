530bc757a5f3f7f8da9e6d7d1cfe9960264e97c2
Shawfe Sung <shawfe@subroutine.name>
2017-04-22 16:18:35 -0400

    Added query parameter 'area_code' (no effect yet)

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 #!Scripts/python
 
 import puller
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import config
 
 app = Flask(__name__)
 
-@app.route( "/bad_numbers" )
+@app.route( "/v1.0/bad_numbers" )
 def all_phone_numbers():
-    badnumber_list = [ x.__dict__ 
-        for x in puller.pull_page( config.source_url ) ]
+    area_code = request.args.get( "area_code" )
+    if area_code != None:
+        badnumber_list = []
+    else:
+        badnumber_list = [ x.__dict__
+            for x in puller.pull_page( config.source_url ) ]
     return jsonify( badnumber_list )
-    
-    
+
 if __name__ == "__main__":
     app.run( debug = True )
